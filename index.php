acf16be80528df56a874ac333a9589ad87684604
Alperen Çerezci <alperen@edakik.com>
2020-06-06 20:37:56 +0300

    table fixed

--- index.php
+++ index.php
@@ -5,30 +5,15 @@ const DEFAULT_URL = 'https://amnezi-a57aa.firebaseio.com/';
 const DEFAULT_TOKEN = 'EXVtuP92GwuahdY9x4Pd4Ldo8vRdGR18m2n34P4F';
 const DEFAULT_PATH = '/firebase/example';
 
-print("HELLO AQ");
-
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
-
-// --- storing an array ---
-// $test = [
-//     'foo' => 'bar',
-//     'i_love' => 'lamp',
-//     'id' => 42
-// ];
 $dateTime = new DateTime();
-// $firebase->set(DEFAULT_PATH . '/' . $dateTime->format('c'), $test);
-
-
-// --- storing a string ---
-// $firebase->push(DEFAULT_PATH . '/name/contact001', 'John Doe');
-
-// --- reading the stored string ---
-// $name = $firebase->get(DEFAULT_PATH . '/name/contact001');
 
 if($_GET){
     $lat = $_GET['lat'];
+    $lat = $_GET['long'];
 
-    $firebase->push(DEFAULT_PATH . '/name/erkanyarraki', $lat);
+    $firebase->push(DEFAULT_PATH . '/location/lat', $lat);
+    $firebase->push(DEFAULT_PATH . '/location/long', $long);
 }
 
 ?>
