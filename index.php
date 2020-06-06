e1a4a3c41094874f0ad9cf54d85b10510739778f
Alperen Çerezci <alperen@edakik.com>
2020-06-06 20:08:13 +0300

    second

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ const DEFAULT_URL = 'https://amnezi-a57aa.firebaseio.com/';
 const DEFAULT_TOKEN = 'EXVtuP92GwuahdY9x4Pd4Ldo8vRdGR18m2n34P4F';
 const DEFAULT_PATH = '/firebase/example';
 
+print("HELLO AQ");
+
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
 // --- storing an array ---
