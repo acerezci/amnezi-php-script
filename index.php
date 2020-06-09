c215c5ebb3a00ba43fa4572d5b9978861f2bb746
Alperen Çerezci <alperen@edakik.com>
2020-06-09 21:59:48 +0300

    fixed get

--- index.php
+++ index.php
@@ -3,17 +3,14 @@ require './vendor/autoload.php';
 
 const DEFAULT_URL = 'https://amnezi-a57aa.firebaseio.com/';
 const DEFAULT_TOKEN = 'EXVtuP92GwuahdY9x4Pd4Ldo8vRdGR18m2n34P4F';
-const DEFAULT_PATH = '/firebase/example';
 
 print("HELLO");
 
 $firebase = new \Firebase\FirebaseLib(DEFAULT_URL, DEFAULT_TOKEN);
 
 if($_GET){
-    $lat = $_GET['lat'];
-    $long = $_GET['long'];
+    $location = $_GET['location'];
 
-    $firebase->push(DEFAULT_PATH . '/location/lat', $lat);
-    $firebase->push(DEFAULT_PATH . '/location/long', $long);
+    $firebase->push('/location', $location);
 }
 ?>
